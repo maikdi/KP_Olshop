122d22c568401b451805dbc0bbbcbcdff8f97b60
Valentiana <vreszisca@gmail.com>
2021-12-09 23:30:29 +0700

    change navbar color

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="welcome">
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid">
 <a class="navbar-brand" href="#">Navbar</a>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
