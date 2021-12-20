eb6cfa8a6175ad377f24fcffc01639ed5eb83f0b
Michael David <mikedh2612@gmail.com>
2021-12-20 23:59:42 +0700

    Initial Commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../views/Login.vue'
-import Welcome from '../views/Welcome.vue'
+import Dashboard from '../views/Dashboard.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -11,9 +11,9 @@ const routes = [
     component: Login
   },
   {
-    path: '/welcome',
-    name: 'Welcome',
-    component: Welcome
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   }
 ]
 
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,11 +3,19 @@
     <Navbar></Navbar>
     <div class="row row-cols-4 row-cols-md-4 g-4">
       <div class="col" v-for="product in products" :key="product[0]">
-        <div class="card" style="width: 18rem">
+        <div class="card" style="width: 18rem; margin-top:10px">
           <img src="" class="card-img-top" alt="" />
           <div class="card-body">
-            <p class="card-title">{{ product[1] }}</p>
-            <p class="card-text">Harga: {{ product[2] }}</p>
+            <p class="card-title"><b>{{ product[1] }}</b></p>
+            <p class="card-text">
+              Harga:
+              {{
+                new Intl.NumberFormat("in-IN", {
+                  style: "currency",
+                  currency: "IDR",
+                }).format(product[2])
+              }}
+            </p>
             <a href="#" class="btn btn-primary">Details</a>
             <a href="#" class="btn btn-success">Add to cart</a>
           </div>
@@ -19,11 +27,10 @@
 
 <script>
 export default {
-  name: "Login",
+  name: "Dashboard",
   data() {
     return {
       products: "",
-      valid: true,
     };
   },
   created() {
